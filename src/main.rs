d800b460b6c7e35e980fc23f45a4772364f5ca38
Maurycy Z <10maurycy10@gmail.com>
2021-10-26 13:55:14 -0700

    add line wraping

--- src/main.rs
+++ src/main.rs
@@ -4,30 +4,61 @@ use std::io::BufReader;
 use std::io::BufRead;
 extern crate clap;
 use clap::{Arg, App};
+use termion;
+use std::cmp::min;
+
+fn push_line_to_buble_buffer(buffer: &mut String, text: &str, padding: &str, tw: usize) {    
+    buffer.push_str(padding);
+    buffer.push_str("< ");
+    buffer.push_str(text);
+    let pad = tw-(text.len()+3);
+    for _ in 0..pad {
+        buffer.push(' ');
+    }
+    buffer.push_str(" >\n");
+}
 
 fn generate_buble(pos: i32, text: &str) -> String {
+    
+    let terminal_size = match termion::terminal_size() {Ok(x) => Some(x), Err(_) => None};
+    
+    let terminal_w = terminal_size.map(|x| x.0);
+    
     let mut padding = String::new();
     for _i in 1..pos {
         padding.push(' ');
     }
     
+    let tw = match terminal_w {
+        Some(w) => min(text.len() + 4,w as usize - padding.len() - 1),
+        None => text.len()
+    };
+    
     let mut buffer = String::new();
-    let len = text.len();
     
     buffer.push_str(" ");
-    for _i in 0..(len+2) {
+    for _i in 0..(tw-2) {
         buffer.push('_');
     }
     buffer.push('\n');
     
-    buffer.push_str(&*padding);
-    buffer.push_str("< ");
-    buffer.push_str(text);
-    buffer.push_str(" >\n");
+    let mut pos = 0;
+
+    match terminal_w {
+        Some(w) => {
+            while pos < text.len() {
+                let w_overhead = (padding.len() + 4) as usize;
+                let len = min(text.len()-pos, w as usize - w_overhead);
+                push_line_to_buble_buffer(&mut buffer,&text[pos..(pos+len)],&padding,tw);
+                pos += len
+            }
+        },
+        None => push_line_to_buble_buffer(&mut buffer,text,&padding,tw),
+    }
     
     buffer.push_str(&*padding);
     buffer.push_str(" ");
-    for i in 0..(len+2) {
+    for i in 0..(tw-2) {
         buffer.push('-');
     }
     
